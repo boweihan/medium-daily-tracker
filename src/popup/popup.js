b7e53345cbbbbda7f590e6db8dfb2c80b3d510a5
bowei <bowei.han100@gmail.com>
2020-10-01 00:34:15 -0400

    popup refactor

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,76 +1,62 @@
-// dynamic elements
-const $version = document.querySelector("#version");
-const $posts = document.querySelector("#posts");
-const $spinner = document.querySelector("#spinner");
+/********* selectors **********/
+const $body = document.querySelector("#body");
+const $spinner = document.querySelector(".spinner-container");
 
-// set extension version to be manifest version
-const setVersion = () => {
-  $version.textContent = "v" + chrome.runtime.getManifest().version;
-};
-
-const createListTitle = (post) => {
-  const title = document.createElement("p");
-  const textNode = document.createTextNode(post.title);
-  title.appendChild(textNode);
-  return title;
-};
-
-const createParagraphText = (text) => {
-  const paragraph = document.createElement("p");
-  const textNode = document.createTextNode(text);
-  paragraph.appendChild(textNode);
-  return paragraph;
+/******** DOM helpers *********/
+const createText = (text) => {
+  const $text = document.createElement("p");
+  const $textNode = document.createTextNode(text);
+  $text.appendChild($textNode);
+  return $text;
 };
 
 const createListMetrics = (post) => {
-  const listMetric = document.createElement("div");
-  listMetric.appendChild(createParagraphText("views: " + post.views));
-  listMetric.appendChild(createParagraphText("reads: " + post.reads));
-  listMetric.appendChild(createParagraphText("upvotes: " + post.upvotes));
-  listMetric.appendChild(createParagraphText("claps: " + post.claps));
-  return listMetric;
-};
-
-const createListItem = (post, total) => {
-  const listItem = document.createElement("li");
-  const container = document.createElement("div");
-  container.appendChild(createListTitle(post));
-  container.appendChild(createListMetrics(post));
-  listItem.appendChild(container);
-  listItem.appendChild(createListBar(post, total));
-
-  return listItem;
+  const $metric = document.createElement("div");
+  $metric.appendChild(createText(`views: ${post.views}`));
+  $metric.appendChild(createText(`reads: ${post.reads}`));
+  $metric.appendChild(createText(`upvotes: ${post.upvotes}`));
+  $metric.appendChild(createText(`claps: ${post.claps}`));
+  return $metric;
 };
 
 const createListBar = (post, total) => {
-  const bar = document.createElement("span");
-  const mask = document.createElement("span");
-  bar.classList.add("bar");
-  mask.classList.add("mask");
-  mask.style.left = `${Math.round((post.views / total) * 100)}%`;
-  mask.style.right = 0;
-  bar.appendChild(mask);
-  return bar;
+  const $bar = document.createElement("span");
+  const $mask = document.createElement("span");
+  $mask.style.left = `${Math.round((post.views / total) * 100)}%`;
+  $bar.appendChild($mask);
+  return $bar;
 };
 
-// fetch post statistics and display
-const appendPostListItems = (posts, total) => {
-  const fragment = document.createDocumentFragment();
+const createListItem = (post, total) => {
+  const $item = document.createElement("li");
+  $item.appendChild(createText(post.title));
+  $item.appendChild(createListMetrics(post));
+  $item.appendChild(createListBar(post, total));
+  return $item;
+};
+
+/**
+ * Render a list of DOM elements based on normalized data
+ * @param {*} data
+ */
+const render = (data) => {
+  const { posts, total } = data;
+  const $fragment = document.createDocumentFragment();
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
-    fragment.appendChild(createListItem(post, total));
+    $fragment.appendChild(createListItem(post, total));
   }
-  $posts.appendChild(fragment);
+  $body.appendChild($fragment);
 };
 
-const setPostStats = () => {
-  chrome.runtime.sendMessage({ type: "GET_POST_STATS" }, {}, (res) => {
-    const { posts, total } = res;
-    appendPostListItems(posts, total);
+/**
+ * Fetch normalized data from background script
+ */
+const initialize = () => {
+  chrome.runtime.sendMessage({ type: "GET_POST_STATS" }, {}, (data) => {
+    render(data);
     $spinner.style.display = "none";
   });
 };
 
-// initialization
-setVersion();
-setPostStats();
+initialize();
